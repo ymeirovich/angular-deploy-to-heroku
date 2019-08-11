440cd5a43da8b62da187ff5a3d0c8a3cafe87302
Yitzchak Meirovich <ymeirovich@gmail.com>
2019-08-11 10:47:01 +0300

    update server.js

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/angular-deploy-to-heroku'));
+app.use(express.static(__dirname + '/dist/tensult-angular-deploy-heroku'));
 
 app.get('/*', function(req, res) {
 
-    res.sendFile(path.join(__dirname + '/dist/angular-deploy-to-heroku/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/tensult-angular-deploy-heroku/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
